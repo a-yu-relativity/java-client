496d1fa71756fec50a704ec5ab69482f41182011
Albert Yu <albert.yu@relativity.com>
2018-08-15 09:32:38 -0500

    Add samples class

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
 public class Main
 {
-	public static void main(String[] args)
-	{
-		RelativityClient client = new RelativityClient();
-		client.get("https://www.example.com", 5000);
-	}
+    public static void main(String[] args)
+    {
+        // RelativityClient client = new RelativityClient();
+        // client.get("https://www.example.com", 5000);
+
+        int expectedNumOfArgs = 3;
+        if (args.length != expectedNumOfArgs)
+        {
+            System.out.println("Need three arguments: instance URL, username, password");
+            return;
+        }
+
+        String url = args[0];
+        String username = args[1];
+        String password = args[2];
+
+        RelativityClient relClient = new RelativityClient(url, username, password);
+    }
 }
---
+++ Samples.java
@@ -0,0 +1,8 @@
+public class Samples
+{
+	// retrieve the first 25 documents from a workspace
+	public static void BasicQuery(RelativityClient relClient, int workspaceId)
+	{
+		
+	}
+}
