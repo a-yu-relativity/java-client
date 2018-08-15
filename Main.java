69cd3e9ef826f90c23dfc02012edcbe064292550
Albert Yu <albert.yu@relativity.com>
2018-08-15 14:53:10 -0500

    Parameterize workspace ID

--- Main.java
+++ Main.java
@@ -5,19 +5,20 @@ public class Main
         // RelativityClient client = new RelativityClient();
         // client.get("https://www.example.com", 5000);
 
-        int expectedNumOfArgs = 3;
+        int expectedNumOfArgs = 4;
         if (args.length != expectedNumOfArgs)
         {
-            System.out.println("Need three arguments: instance URL, username, password");
+            System.out.println(
+                "Need four arguments: instance URL, username, password, workspace ID");
             return;
         }
 
         String url = args[0];
         String username = args[1];
         String password = args[2];
+        int workspaceId = Integer.parseInt(args[3]);
 
         RelativityClient relClient = new RelativityClient(url, username, password);
-        int workspaceId = 6578147;
         Samples.basicQuery(relClient, workspaceId);
     }
 }
